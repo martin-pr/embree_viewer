06ead179fd32584fbc37b1ee8b7cad9bbc025635
Martin <martin.prazak@gmail.com>
2018-03-04 16:38:21 +0000

    Upgraded to SDL2

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include <stdexcept>
 #include <fstream>
 
-#include <SDL/SDL.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_render.h>
 
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
@@ -16,19 +17,6 @@
 #include "scene_loading.h"
 
 #define SCREEN_SIZE	512
-#define SCREEN_BPP	32
-
-namespace {
-void set_pixel(SDL_Surface *surface, int x, int y, const Vec3& pixel) {
-	Uint32 *target_pixel = (Uint32*)((Uint8 *)surface->pixels + y * surface->pitch +
-	                                 x * sizeof(* target_pixel));
-
-	Uint32 value = ((Uint32)(pixel.x * 255) << 16) + ((Uint32)(pixel.y * 255) << 8) +
-	               ((Uint32)(pixel.z * 255) << 0) + (255 << 24);
-
-	*target_pixel = value;
-}
-}
 
 namespace po = boost::program_options;
 
@@ -55,7 +43,10 @@ int main(int argc, char* argv[]) {
 		throw std::runtime_error(SDL_GetError());
 
 	// make the window
-	SDL_Surface* screen = SDL_SetVideoMode(SCREEN_SIZE, SCREEN_SIZE, SCREEN_BPP, SDL_SWSURFACE | SDL_RESIZABLE);
+	SDL_Window* screen = SDL_CreateWindow("embree_viewer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_SIZE, SCREEN_SIZE, SDL_SWSURFACE | SDL_WINDOW_RESIZABLE);
+	SDL_Renderer* renderer = SDL_CreateRenderer(screen, -1, 0);
+
+	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_GetWindowSurface(screen)->format->format, SDL_TEXTUREACCESS_STREAMING, SCREEN_SIZE, SCREEN_SIZE);
 
 	{
 		// make the scene
@@ -93,22 +84,38 @@ int main(int argc, char* argv[]) {
 		bool quit = false;
 
 		while(!quit) {
-			// render pixels
-			for(int y = 0; y < screen->h; ++y)
-				for(int x = 0; x < screen->w; ++x) {
-					const float xf = ((float)x / (float)screen->w - 0.5f) * 2.0f;
-					const float yf = ((float)y / (float)screen->h - 0.5f) * 2.0f;
-					const float aspect = (float)screen->w / (float)screen->h;
+			// // render pixels
+			Uint32* pixels = nullptr;
+			int pitch = 0;
+			Uint32 format;
+
+			int w, h;
+			SDL_QueryTexture(texture, &format, NULL, &w, &h);
+
+			if(SDL_LockTexture(texture, nullptr, (void**)&pixels, &pitch))
+				throw std::runtime_error(SDL_GetError());
+
+			for(int y = 0; y < h; ++y)
+				for(int x = 0; x < w; ++x) {
+					const float xf = ((float)x / (float)w - 0.5f) * 2.0f;
+					const float yf = ((float)y / (float)h - 0.5f) * 2.0f;
+					const float aspect = (float)w / (float)h;
 
 					const Ray r = cam.makeRay(xf, -yf / aspect);
 
 					const Vec3 color = scene.renderPixel(r);
 
-					set_pixel(screen, x, y, color);
+					Uint32 rgb = SDL_MapRGBA(SDL_GetWindowSurface(screen)->format, (Uint8)(color.x * 255.0), (Uint8)(color.y * 255.0), (Uint8)(color.z * 255.0), 255);
+					Uint32 pixelPosition = y * (pitch / sizeof(Uint32)) + x;
+					pixels[pixelPosition] = rgb;
 				}
 
+			SDL_UnlockTexture(texture);
+
 			// show the result by flipping the double buffer
-			SDL_Flip(screen);
+			SDL_RenderCopy(renderer, texture, NULL, NULL);
+
+			SDL_RenderPresent(renderer);
 
 			// exit condition
 			{
@@ -121,14 +128,14 @@ int main(int argc, char* argv[]) {
 					// camera motion
 					else if(event.type == SDL_MOUSEMOTION) {
 						if(event.motion.state & SDL_BUTTON_LMASK) {
-							const float xangle = ((float)(event.motion.xrel) / (float)screen->w) * M_PI;
-							const float yangle = ((float)(event.motion.yrel) / (float)screen->h) * M_PI;
+							const float xangle = ((float)(event.motion.xrel) / (float)w) * M_PI;
+							const float yangle = ((float)(event.motion.yrel) / (float)h) * M_PI;
 
 							cam.rotate(xangle, -yangle);
 						}
 
 						else if(event.motion.state & SDL_BUTTON_RMASK) {
-							const float ydiff = ((float)(event.motion.yrel) / (float)screen->h);
+							const float ydiff = ((float)(event.motion.yrel) / (float)h);
 
 							Vec3 tr = cam.target - cam.position;
 							float dist = tr.length();
@@ -141,8 +148,12 @@ int main(int argc, char* argv[]) {
 					}
 
 					// window resizing
-					else if(event.type == SDL_VIDEORESIZE) {
-						screen = SDL_SetVideoMode(event.resize.w, event.resize.h, SCREEN_BPP, SDL_SWSURFACE | SDL_RESIZABLE);
+					else if(event.type == SDL_WINDOWEVENT) {
+						if (event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
+							SDL_DestroyTexture(texture);
+
+							texture = SDL_CreateTexture(renderer, SDL_GetWindowSurface(screen)->format->format, SDL_TEXTUREACCESS_STREAMING, event.window.data1, event.window.data2);
+						}
 					}
 				}
 			}
@@ -152,7 +163,8 @@ int main(int argc, char* argv[]) {
 	}
 
 	// clean up
-	SDL_FreeSurface(screen);
+	SDL_DestroyRenderer(renderer);
+
 	SDL_Quit();
 
 	return 0;
