53f85b69bfb21363e0b65216db1467b9dc78592e
Martin Prazak <martin.prazak@gmail.com>
2018-03-04 20:10:48 +0000

    Event loop now changed to passive waiting (no CPU overhead when no movement)

--- src/main.cpp
+++ src/main.cpp
@@ -91,53 +91,23 @@ int main(int argc, char* argv[]) {
 		bool quit = false;
 
 		while(!quit) {
-			// // render pixels
-			Uint32* pixels = nullptr;
-			int pitch = 0;
-			Uint32 format;
-
-			int w, h;
-			SDL_QueryTexture(texture, &format, NULL, &w, &h);
-
-			if(SDL_LockTexture(texture, nullptr, (void**)&pixels, &pitch))
-				throw std::runtime_error(SDL_GetError());
-
-			for(int y = 0; y < h; ++y)
-				for(int x = 0; x < w; ++x) {
-					const float xf = ((float)x / (float)w - 0.5f) * 2.0f;
-					const float yf = ((float)y / (float)h - 0.5f) * 2.0f;
-					const float aspect = (float)w / (float)h;
-
-					const Ray r = cam.makeRay(xf, -yf / aspect);
-
-					const Vec3 color = scene.renderPixel(r);
-
-					Uint32 rgb = SDL_MapRGBA(SDL_GetWindowSurface(screen)->format, (Uint8)(color.x * 255.0), (Uint8)(color.y * 255.0), (Uint8)(color.z * 255.0), 255);
-					Uint32 pixelPosition = y * (pitch / sizeof(Uint32)) + x;
-					pixels[pixelPosition] = rgb;
-				}
-
-			SDL_UnlockTexture(texture);
-
-			// show the result by flipping the double buffer
-			SDL_RenderCopy(renderer, texture, NULL, NULL);
-
-			SDL_RenderPresent(renderer);
-
-			// exit condition
+			/////////////////////
+			// EVENT LOOP
+			/////////////////////
 			{
 				SDL_Event event;
-				while(SDL_PollEvent(&event)) {
+				SDL_WaitEvent(&event);
+				do {
 					// quit
 					if(event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE))
 						quit = true;
 
 					// camera motion
 					else if(event.type == SDL_MOUSEMOTION) {
-						if(event.motion.state & SDL_BUTTON_LMASK) {
-							int w, h;
-							SDL_GetWindowSize(screen, &w, &h);
+						int w, h;
+						SDL_GetWindowSize(screen, &w, &h);
 
+						if(event.motion.state & SDL_BUTTON_LMASK) {
 							const float xangle = ((float)(event.motion.xrel) / (float)w) * M_PI;
 							const float yangle = ((float)(event.motion.yrel) / (float)h) * M_PI;
 
@@ -162,10 +132,47 @@ int main(int argc, char* argv[]) {
 						if (event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
 							SDL_DestroyTexture(texture);
 
-							texture = SDL_CreateTexture(renderer, SDL_GetWindowSurface(screen)->format->format, SDL_TEXTUREACCESS_STREAMING, event.window.data1, event.window.data2);
+							texture = SDL_CreateTexture(renderer, SDL_GetWindowSurface(screen)->format->format, SDL_TEXTUREACCESS_STREAMING, event.window.data1 / 4, event.window.data2 / 4);
 						}
 					}
-				}
+				} while(SDL_PollEvent(&event));
+			}
+
+			/////////////////////
+			// RENDERING
+			/////////////////////
+			{
+				Uint32* pixels = nullptr;
+				int pitch = 0;
+				Uint32 format;
+
+				int w, h;
+				SDL_QueryTexture(texture, &format, NULL, &w, &h);
+
+				if(SDL_LockTexture(texture, nullptr, (void**)&pixels, &pitch))
+					throw std::runtime_error(SDL_GetError());
+
+				for(int y = 0; y < h; ++y)
+					for(int x = 0; x < w; ++x) {
+						const float xf = ((float)x / (float)w - 0.5f) * 2.0f;
+						const float yf = ((float)y / (float)h - 0.5f) * 2.0f;
+						const float aspect = (float)w / (float)h;
+
+						const Ray r = cam.makeRay(xf, -yf / aspect);
+
+						const Vec3 color = scene.renderPixel(r);
+
+						Uint32 rgb = SDL_MapRGBA(SDL_GetWindowSurface(screen)->format, (Uint8)(color.x * 255.0), (Uint8)(color.y * 255.0), (Uint8)(color.z * 255.0), 255);
+						Uint32 pixelPosition = y * (pitch / sizeof(Uint32)) + x;
+						pixels[pixelPosition] = rgb;
+					}
+
+				SDL_UnlockTexture(texture);
+
+				// show the result by flipping the double buffer
+				SDL_RenderCopy(renderer, texture, NULL, NULL);
+
+				SDL_RenderPresent(renderer);
 			}
 
 			++ctr;
