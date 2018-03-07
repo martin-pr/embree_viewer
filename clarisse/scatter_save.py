8c6e9531da7504e6f005a310d1d6865b90b317e9
Martin Prazak <martin.prazak@gmail.com>
2018-03-07 00:20:10 +0000

    Make sure no scatterers are saved more than once

--- clarisse/scatter_save.py
+++ clarisse/scatter_save.py
@@ -2,9 +2,10 @@ import ix
 import os
 import json
 import struct
+from sets import Set
 
 # set to True to only leave "leaf" filenames (not full paths) in the file
-STRIP_FILENAMES = True
+STRIP_FILENAMES = False
 # maximum number of instanced objects inlined in a file
 MAX_INLINE_INSTANCES = 10
 
@@ -15,7 +16,7 @@ def mat2arr(mat):
     return result
 
 
-def scan(path, target_dir):
+def scan(path, target_dir, saved_scatterers):
     result = []
 
     obj = ix.application.get_factory().get_item(path)
@@ -42,7 +43,7 @@ def scan(path, target_dir):
 
             objs = []
             for i in range(0, base.get_count()):
-                ii = scan(base[i].get_object().get_full_name(), target_dir)
+                ii = scan(base[i].get_object().get_full_name(), target_dir, saved_scatterers)
                 objs.append(ii)
             subscene["objects"] = objs;
             subscene["transform"] = mat2arr(mod.get_object_matrix())
@@ -68,12 +69,15 @@ def scan(path, target_dir):
                 else:
                     filename = obj.get_full_name().replace('/', '__').replace(':', '__') + '.bin'
 
-                    instance_file = open(target_dir + '/' + filename, "wb")
-                    for i in range(0, inst.get_count()):
-                        instance_file.write(struct.pack('I', inst[i]))
-                        instance_file.write(struct.pack('16f', *mat2arr(mats[i])))
+                    if not filename in saved_scatterers:
+                        saved_scatterers.add(filename)
 
-                    subscene["instance_file"] = filename
+                        instance_file = open(target_dir + '/' + filename, "wb")
+                        for i in range(0, inst.get_count()):
+                            instance_file.write(struct.pack('I', inst[i]))
+                            instance_file.write(struct.pack('16f', *mat2arr(mats[i])))
+
+                        subscene["instance_file"] = filename
 
             result.append(subscene)
 
@@ -83,14 +87,15 @@ def scan(path, target_dir):
         items = ix.api.OfItemVector()
         ctx.get_items(items)
         for o in items:
-            items = scan(o.get_full_name(), target_dir)
+            items = scan(o.get_full_name(), target_dir, saved_scatterers)
             if items:
                 result = result + items
 
     return result
 
 def writeData(path, target_dir):
-    result = scan(path, target_dir)
+    saved_scatterers = Set()
+    result = scan(path, target_dir, saved_scatterers)
 
     with open(target_dir + '/scene.json', 'w') as outfile:
         json.dump(result, outfile, indent=4)
